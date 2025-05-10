4be7a0da568dd33eeef68569be2f401733f3d392
alexanderyakovenko <yakovenko5893@gmail.com>
2025-05-10 07:52:58 +0200

    map add

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('info/', views.info, name='info')
+    path('info/', views.info, name='info'),
+    path('menu/', views.menu, name='menu'),
+    path('poi/', views.poi, name='poi')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import folium
 
 # Create your views here.
 def index(request):
@@ -9,3 +10,29 @@ def index(request):
 # info for driver
 def info(request):
     return render(request, 'info.html')
+
+# menu bar
+def menu(request):
+    return render(request, 'menu.html')
+
+# POI
+def poi(request):
+    #m = folium.Map(location=[49.147417, 8.551702], zoom_start=9)()
+    #context = {'map': m._repr_html_()}
+
+    m = folium.Map(location=[49.147417, 8.551702], zoom_start=9)
+
+    folium.Marker(
+        location=[49.148157,8.553572],
+        tooltip="Base Bruchsal",
+        popup="49.148157,8.553572",
+        icon=folium.Icon(color="green"),
+    ).add_to(m)
+
+    m = m._repr_html_()
+    context = {
+        'm': m,
+    }
+
+    return render(request, 'poi.html', context )
+
